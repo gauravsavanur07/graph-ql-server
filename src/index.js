9d87905311ba86a2728329ad65a52e5f8ef7168a
Gaurav Savanur <gauravsavanur07@gmail.com>
2018-06-22 12:34:25 +0530

    Filtering,Pagination and Sorting

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const AuthPayload = require('./resolvers/AuthPayload')
 const Subscription = require('./resolvers/Subscription')
+const Feed = require('./resolvers/Feed')
 
 // 1
 `
@@ -20,6 +21,7 @@ const resolvers = {
   Mutation,
   AuthPayload,
   Subscription,   
+  Feed
 
  },
 }
